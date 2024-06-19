f3675342d93ff7623b4f8cf48ff577de76d5ffbf
Máximo Sallemi <msallemi@mail.austral.edu.ar>
2024-06-19 18:16:36 -0300

    si

--- src/test/java/edu/austral/ingsis/MyFileSystemRunner.java
+++ src/test/java/edu/austral/ingsis/MyFileSystemRunner.java
@@ -3,10 +3,10 @@ package edu.austral.ingsis;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileSystemRunnerClass implements FileSystemRunner {
+public class MyFileSystemRunner implements FileSystemRunner {
   private final edu.austral.ingsis.clifford.FileSystem fileSystem;
 
-  public FileSystemRunnerClass(edu.austral.ingsis.clifford.FileSystem fileSystem) {
+  public MyFileSystemRunner(edu.austral.ingsis.clifford.FileSystem fileSystem) {
     this.fileSystem = fileSystem;
   }
 
